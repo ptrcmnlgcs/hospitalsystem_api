a37f07e1e2414259f0f5e58ed19b677bfa43adcf
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-06-13 02:20:36 +0800

    Add api for users

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+
+    Route::get('user', [UserController::class, 'index']);
+    Route::post('user', [UserController::class,'store']);
+    Route::get('user/{id}', [UserController::class,'show']);
+    Route::put('user/{id}', [UserController::class,'update']);
+
+});
