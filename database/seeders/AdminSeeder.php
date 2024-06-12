f3af2c325fe1a6ea4b437196c669d2dff9d68f12
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-06-13 02:20:03 +0800

    Add seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('Password#123'),
+            'userType' => 'admin', 
+        ]);
+    }
+}
